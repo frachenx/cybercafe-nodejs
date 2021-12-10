193d5f0469cab6ffd4f60ac5ec0ee7672c292a11
Hafid <h91939@hotmail.com>
2021-12-09 20:38:23 -0600

    deleted php files

--- server/routes/computers.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once("../classes/computer.php");
-require_once("../config.php");
-
-$computers =Compututer::getComputers();
-echo json_encode($computers);
--- server/routes/report.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once("../config.php");
-require_once("../classes/user.php");
-User::report($_GET['start'],$_GET['end']);
-// echo $_GET['start'] . "   " .$_GET['end'];
